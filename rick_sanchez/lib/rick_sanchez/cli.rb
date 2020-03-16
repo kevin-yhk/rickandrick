4ba569916a9398ca83a0cad799fe05bc5af804f5
Kevin Kim <36459380+kevin-yhk@users.noreply.github.com>
2020-03-15 18:19:21 -0700

    Update cli.rb

--- rick_sanchez/lib/rick_sanchez/cli.rb
+++ rick_sanchez/lib/rick_sanchez/cli.rb
@@ -1,16 +1,29 @@
-require 'pry'
-require_relative "./rick_api.rb"
-require_relative "./rick.rb"
-class CLI 
+class RickSanchez::CLI 
 
-    def self.call 
+    def call 
         
-        API.get_ricks
-         
-
+      RickSanchez::API.get_ricks
+        
+      puts "          
+        . -^   `--,      
+       /# =========`-_   
+      /# (--====___====\ 
+     /#   .- --.  . --.| 
+    /##   |  * ) (   * ),
+    |##   \    /\ \   / |
+    |###   ---   \ ---  |
+    |####      ___)    #|
+    |######           ##|
+     \##### ---------- / 
+      \####           (  
+       `\###          |  
+         \###         |  
+          \##        |   
+           \###.    .)   
+            `======/   "            
+        puts "Wubbalubbadubdub! There are 20 Rick's from different universes on the loose! SHOW ME WHAT YOU GOT!"
         user_input = ""
         while user_input != "exit" do
-        puts "Wubbalubbadubdub! There are 20 Rick's from different universes on the loose!"
         puts "To find out who these Rick's are, enter 'list names'."
         puts "To find a specific Rick's information (status, species, origin, location, image, url) enter the entire name!'"
         puts "If you don't care and want to leave them to their own mayhem, enter 'exit'."
@@ -19,14 +32,14 @@ class CLI
            
         case input 
           when "list names"
-              Rick.list_names.each do |rick|
+              RickSanchez::Rick.list_names.each do |rick|
                 puts rick 
               end 
-          when Rick.list_names.select do |rick|
+          when RickSanchez::Rick.list_names.select do |rick|
                 if rick == input 
-                Rick.find_by_name(input)
+                RickSanchez::Rick.find_by_name(input)
                 end 
-                end 
+              end 
           when "exit"
               puts "Goodbye moonman!"
             break

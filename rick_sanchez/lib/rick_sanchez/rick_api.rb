53ca5ec846c887d571c191cee14985efe0af4fcb
Kevin Kim <36459380+kevin-yhk@users.noreply.github.com>
2020-03-15 18:20:42 -0700

    Update rick_api.rb

--- rick_sanchez/lib/rick_sanchez/rick_api.rb
+++ rick_sanchez/lib/rick_sanchez/rick_api.rb
@@ -1,21 +1,13 @@
-require 'pry'
-require 'rest-client'
-require 'json'
-require_relative "./rick.rb"
-class API 
+class RickSanchez::API 
 
 
-def self.get_ricks
-    resp = RestClient.get("https://rickandmortyapi.com/api/character/?page=1&name=rick")
-    response_hash = JSON.parse(resp.body, symbolize_names:true)
-    rickandrick_array = response_hash[:results]
+    def self.get_ricks
+        resp = RestClient.get("https://rickandmortyapi.com/api/character/?page=1&name=rick")
+        response_hash = JSON.parse(resp.body, symbolize_names:true)
+        rickandrick_array = response_hash[:results]
 
-    rickandrick_array.each {|rick|
-    Rick.new(rick[:name], rick[:status], rick[:species], rick[:origin][:name], rick[:location][:name], rick[:image], rick[:url])}
-end 
-
-
-
-
-
+        rickandrick_array.each {|rick|
+        RickSanchez::Rick.new(rick[:name], rick[:status], rick[:species], rick[:origin][:name], rick[:location][:name], rick[:image], rick[:url])}
+        # binding.pry
+    end 
 end 
